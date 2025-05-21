9eff54f93b622e1ce82992cfe7ba267781473738
vpn-web <58022881+vpn-web@users.noreply.github.com>
2025-05-21 20:00:03 +0800

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,10 @@
-// frontend/vite.config.js
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
 export default defineConfig({
-  // 指定入口 HTML 所在目录
   root: 'public',
-  // 生产环境构建配置
   build: {
-    // 将构建输出到 frontend/dist
     outDir: '../dist',
-    // 每次构建前清空输出目录
     emptyOutDir: true
   },
   plugins: [
